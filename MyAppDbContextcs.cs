24e7dd6d38680961c6706a42a8ce1f17b2ebf8e5
jeevkant <103170540+jeevkant@users.noreply.github.com>
2023-04-28 10:31:37 +0530

    Add files via upload

---
+++ MyAppDbContextcs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalCrudWithApi.Model;
+
+namespace FinalCrudWithApi
+{
+    public class MyAppDbContextcs : DbContext
+    {
+        public MyAppDbContextcs(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> test_employees { get; set; }
+    }
+}
